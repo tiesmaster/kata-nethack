c1fc5d7e5c02d07bf03c195867b51d5b59a6e0e9
Thijs Brobbel <t.brobbel@coolblue.nl>
2016-09-09 16:49:59 +0200

    Add playertests.

---
+++ KataNetHack.Tests/PlayerSubsystem/PlayerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KataNetHack.Console.PlayerSubsystem;
+
+using Xunit;
+
+namespace KataNetHack.Tests.PlayerSubsystem
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void CanCreateSut()
+        {
+            var sut = new Player(default(Point));
+        }
+    }
+}
